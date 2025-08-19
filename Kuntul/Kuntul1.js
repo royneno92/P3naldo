e8d255759fd3eb5fdd52694b1e63d2151bbcb42b
royneno92 <roysarman22@gmail.com>
2025-08-19 16:16:51 +0700

    Update Kuntul1.js

--- Kuntul/Kuntul1.js
+++ Kuntul/Kuntul1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KUNTUL1
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      3.1
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Kuntul/Kuntul1.js 
 // @downloadURL  https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Kuntul/Kuntul1.js
