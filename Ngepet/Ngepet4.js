5c15f8486715092fa58daf3457dd9be1b327f96e
royneno92 <roysarman22@gmail.com>
2025-10-06 21:00:24 +0700

    Update Ngepet4.js

--- Ngepet/Ngepet4.js
+++ Ngepet/Ngepet4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ROY4
 // @namespace    http://tampermonkey.net/
-// @version      3.4
+// @version      3.5
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/royneno92/Cuanaja/refs/heads/main/Ngepet/Ngepet4.js
 // @downloadURL  https://raw.githubusercontent.com/royneno92/Cuanaja/refs/heads/main/Ngepet/Ngepet4.js
@@ -32,8 +32,8 @@ var namagroup6 = 'JNETOTO';
 var Comment6 = '#JNETOTO(6TAMBAH6)*19*16*44';
 var namagroup7 = 'DIVA4D';
 var Comment7 = '#DIVA4D (7TAMBAH7) = 79*75*68';
-var namagroup8 = 'G41B';
-var Comment8 = 'GAIB4D=8TAMBAH8=79*75*68';
+var namagroup8 = 'MAYAPADA';
+var Comment8 = 'BETT*Mayapada4D(BO)*8TAMBAH8*79*75*68';
 var namagroup9 = 'TIKTAKTOGEL';
 var Comment9 = 'Tiktaktogel / 9TAMBAH9 / 93 , 25 , 91';
 var namagroup10 = '453P';
