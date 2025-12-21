fc9c644c19bf5eac6790c769f07445266fa256ba
royneno92 <roysarman22@gmail.com>
2025-12-21 08:41:34 +0700

    Update Ngepet4.js

--- Ngepet/Ngepet4.js
+++ Ngepet/Ngepet4.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      3.7
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/royneno92/Cuanaja/refs/heads/main/Ngepet/Ngepet4.js
-// @downloadURL  https://raw.githubusercontent.com/royneno92/Cuanaja/refs/heads/main/Ngepet/Ngepet4.js
+// @updateURL    https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Ngepet/Ngepet4.js
+// @downloadURL  https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Ngepet/Ngepet4.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
@@ -61,7 +61,7 @@ var Comment18 = 'Ngepet 4';
 
 var SCRIPT_NAME = Comment18
 var refresh = 40;
-var URLADMIN = "https://raw.githubusercontent.com/royneno92/Cuanaja/refs/heads/main/Admin.json"
+var URLADMIN = "https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Admin.json"
 var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori","klikh4tori001"]
 var Backlist =["pemenang","rekap","natidulu","prediksi","result","juara","r3k4p","r3kap","rek4p" ]
 var isCommenting = false;
