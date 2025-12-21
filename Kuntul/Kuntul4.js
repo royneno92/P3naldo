df33609247e6906ce15c30297f413bb6dc0e4115
royneno92 <roysarman22@gmail.com>
2025-12-21 08:44:03 +0700

    Update Kuntul4.js

--- Kuntul/Kuntul4.js
+++ Kuntul/Kuntul4.js
@@ -63,7 +63,7 @@ var Comment18 = 'Kuntul 4';
 
 var SCRIPT_NAME = Comment18
 var refresh = 40;
-var URLADMIN = "https://raw.githubusercontent.com/royneno92/Cuanaja/refs/heads/main/Admin.json"
+var URLADMIN = "https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Admin.json"
 var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori","klikh4tori001"]
 var Backlist =["pemenang","rekap","natidulu","prediksi","result","juara","r3k4p","r3kap","rek4p" ]
 var isCommenting = false;
