55eb605aa4eb39f5c3d58dad77f26963bd2ec30e
royneno92 <roysarman22@gmail.com>
2026-01-13 00:25:27 +0700

    Update Kerapu3.js

--- Kerapu/Kerapu3.js
+++ Kerapu/Kerapu3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kerapu3
 // @namespace    http://tampermonkey.net/
-// @version      4.2
+// @version      4.3
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Kerapu/Kerapu3.js
 // @downloadURL  https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Kerapu/Kerapu3.js
@@ -60,7 +60,7 @@ var Comment18 = 'Kerapu 3';
 
 
 var SCRIPT_NAME = Comment18
-var refresh = 60;
+var refresh = 10;
 var URLADMIN = "https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Admin.json"
 var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori","klikh4tori001"]
 var Backlist =["pemenang","rekap","natidulu","prediksi","result","juara","𝗥𝗘𝗞𝗔𝗣","r3kap","rek4p" ]
