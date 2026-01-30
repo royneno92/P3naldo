85b528339fa90624d343ea371ede214e5b3df463
royneno92 <roysarman22@gmail.com>
2026-01-30 12:24:56 +0700

    Update Ngepet4.js

--- Ngepet/Ngepet4.js
+++ Ngepet/Ngepet4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ngepet4
 // @namespace    http://tampermonkey.net/
-// @version      4.3
+// @version      4.4
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Ngepet/Ngepet4.js
 // @downloadURL  https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Ngepet/Ngepet4.js
@@ -452,7 +452,8 @@ function mulaiRefresh() {
     if (myrefresh !== null) return; // Hindari duplikat interval
 
     myrefresh = setInterval(function () {
-        var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
+        var urutkan = document.querySelectorAll(
+  "[data-mcomponent='ServerTextArea'], [data-mcomponent='TextArea']");
         var waktupost = document.getElementsByClassName("native-text");
 
         if (!document.querySelectorAll("[role='presentation']")[0]) {
