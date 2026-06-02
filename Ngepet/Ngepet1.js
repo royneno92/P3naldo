08fc577fa20eda68723a964ab26203583063ff7f
royneno92 <roysarman22@gmail.com>
2026-06-03 01:21:21 +0700

    Update Ngepet1.js

--- Ngepet/Ngepet1.js
+++ Ngepet/Ngepet1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ngepet1
 // @namespace    http://tampermonkey.net/
-// @version      4.7
+// @version      4.8
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Ngepet/Ngepet1.js
 // @downloadURL  https://raw.githubusercontent.com/royneno92/P3naldo/refs/heads/main/Ngepet/Ngepet1.js
@@ -514,7 +514,7 @@ function startAutoTask() {
 }
 
 
-var TELEGRAM_TOKEN = '8396728370:AAHblTLr220NEd9PwS7BzzS5VWGcxix9RK8'; // GANTI
+var TELEGRAM_TOKEN = '8825413477:AAHCWqL0onj3yxN0TNNSeh_TjJug1u4jims'; // GANTI
 var TELEGRAM_CHAT_ID = '-1002717306025'; // GANTI
 
 let lastMessageSent = ""; // lokal per tab/browser
